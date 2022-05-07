f58739bdcc71149fb60785ea48d5960b12ded253
akram09 <a.boutouchent@esi-sba.dz>
2022-05-07 20:22:22 +0200

    Fixing failing tests

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ user: ['Akram', 'Ahmed', 'Amine'] })
+  res.json({ users: ['Akram', 'Ahmed', 'Amine'] })
 })
 
 module.exports = router
