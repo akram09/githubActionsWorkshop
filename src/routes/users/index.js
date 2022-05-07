e162408dcf18dd294f8b2883bcc4dc5f09dca22c
akram09 <a.boutouchent@esi-sba.dz>
2022-05-07 18:21:17 +0200

    Changing from multiple users to one user

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ users: ['Akram', 'Ahmed'] })
+  res.json({ user: 'Akram' })
 })
 
 module.exports = router
