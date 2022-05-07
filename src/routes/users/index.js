ad696c9be559aa21d699788e375dfd12dcbcd90c
akram09 <a.boutouchent@esi-sba.dz>
2022-05-07 18:22:45 +0200

    Fixing

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json({ user: 'Akram' })
+  res.json({ users: ['Akram', 'Ahmed'] })
 })
 
 module.exports = router
